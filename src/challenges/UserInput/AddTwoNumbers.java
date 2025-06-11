6168aa1d4e803094ec6ed5a6ff6a1cea19b150c7
Rohith.B <rohithbrock9164@gmail.com>
2025-06-11 17:27:44 +0530

    feat: add package declarations to UserInput classes
    Add package declarations to UserInput.java and AddTwoNumbers.java to properly organize them in the src.Challenges.UserInput package. Also includes a new Assignment.java file with basic variable assignment example.

--- src/challenges/UserInput/AddTwoNumbers.java
+++ src/challenges/UserInput/AddTwoNumbers.java
@@ -1,3 +1,5 @@
+package src.Challenges.UserInput;
+
 import java.util.Scanner;
 
 public class AddTwoNumbers {
--- src/challenges/UserInput/UserInput.java
+++ src/challenges/UserInput/UserInput.java
@@ -1,3 +1,4 @@
+package src.Challenges.UserInput;
 import java.util.Scanner;
 
 public class UserInput{
